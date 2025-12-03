fb3e21aee4262f321312c6d0eab0ef66269df4f8
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-12-03 11:15:34 +0000

    chore: test

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,6 +1,6 @@
 export const MARKETPLACE_ORG = "ubq-testing" // "ubiquity-os-marketplace";
 export const BOT_ACTORS = ["ubq-testing[bot]"]//["ubiquity-os[bot]", "ubiquity-os-dev[bot]"];
-export const CONSECUTIVE_FAILURE_THRESHOLD = 10;
+export const CONSECUTIVE_FAILURE_THRESHOLD = 5;
 export const ADMINS_TO_TAG =  ["@keyrxng"]//["@0x4007", "@gentlementlegen"];
 export const ISSUE_TITLE = "Kernel Dispatch Workflow Failures Detected";
 export const ISSUE_LABELS = [
